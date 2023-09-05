9af6b195a1ef35df5fc00d462fc90bd47b098ca6
LouisCR04 <louiskariuki04@gmail.com>
2023-09-05 14:51:38 +0300

    Fixed 0 string and -ve numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-l_dig = number % 10
+l_dig = abs(number) % 10
 if l_dig > 5:
-    string = "greater than 5"
+    string = " and is greater than 5"
 elif l_dig == 0:
-    string = "and is 0"
+    string = ""
 else:
-    string = "and is less than 6 and not 0"
-print(f"Last digit of {number} is {l_dig} and is {string}")
+    string = " and is less than 6 and not 0"
+print(f"Last digit of {number} is {l_dig}{string}")
