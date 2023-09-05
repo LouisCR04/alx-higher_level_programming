26450b5edf36e80f731a7c46f4f8bf769afb2f0f
LouisCR04 <louiskariuki04@gmail.com>
2023-09-05 18:50:01 +0300

    Task 1 Flow errors fixed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 l_dig = abs(number) % 10
-if l_dig > 5:
+if l_dig > 5 and number > 0:
     string = " and is greater than 5"
 elif l_dig == 0:
-    string = ""
+    string = " and is 0"
 else:
     string = " and is less than 6 and not 0"
-print(f"Last digit of {number} is {l_dig}{string}")
+if number > -1 or l_dig == 0:
+    print(f"Last digit of {number} is {l_dig}{string}")
+else:
+    print(f"Last digit of {number} is -{l_dig}{string}")
