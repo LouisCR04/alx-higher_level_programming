d83baa5974f3ca9fdcbd79e6e1a3eff6b94128bf
LouisCR04 <louiskariuki04@gmail.com>
2023-09-07 14:15:49 +0300

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,13 @@ def fx():
     if len(argv) == 1:
         print("0 arguments.")
         return
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
+        print("{}: {}".format(1, argv[1]))
+        return
     for i in range(len(argv)):
         if i == 0:
-            print("{} arguments:".format(len(argv)))
+            print("{} arguments:".format(len(argv) - 1))
         else:
             print("{}: {}".format(i, argv[i]))
 
