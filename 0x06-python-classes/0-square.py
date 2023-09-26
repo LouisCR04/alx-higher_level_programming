950788f942fe99805ef64ab29b884528213aea7b
LouisCR04 <louiskariuki04@gmail.com>
2023-09-26 15:12:11 +0300

    Initial Commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+0-square.py: Empty class Square that defines a square
+"""
+class Square:
+    """Empty class square
+    No args and attr
+    """
+    pass
