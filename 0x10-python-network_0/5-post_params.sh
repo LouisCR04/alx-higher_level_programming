9c3da66a373958bfc6959d059228ab0bf17b14c0
LouisCR04 <louiskariuki04@gmail.com>
2024-02-17 12:56:08 +0300

    Task 5: Argument not passed to the script fixed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends POST req... var email & subject passed too
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
