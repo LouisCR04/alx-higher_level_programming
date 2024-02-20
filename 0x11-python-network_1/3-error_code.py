c7b77b3e49db9769a870fe33a63d65bd04d29d1b
LouisCR04 <louiskariuki04@gmail.com>
2024-02-20 18:48:44 +0300

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Displays body of response in utf-8 while managing HTTPError exceptions"""
+
+if __name__ == "__main__":
+    import urllib.error as error
+    import urllib.request as request
+    from sys import argv
+    req = request.Request(argv[1])
+    try:
+        with request.urlopen(req) as r:
+            print(r.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
