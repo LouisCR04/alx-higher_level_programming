6093c609858a4067b0d306b3eb043149766eac5f
LouisCR04 <louiskariuki04@gmail.com>
2024-02-20 19:22:19 +0300

    Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 """Sends a POST req with an email variable"""
 
-
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    values = {'email': argv[2]}
+    req = requests.post(argv[1], data=values)
+    print(req.text)
