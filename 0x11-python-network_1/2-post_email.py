1dc5387204fd6bda457882a9ec6627aec2eb75cb
LouisCR04 <louiskariuki04@gmail.com>
2024-02-20 18:54:11 +0300

    Added the print statement in T2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,12 +2,12 @@
 """Takes in URL & email,sends a POST req & displays body of response(utf-8)"""
 
 if __name__ == "__main__":
-    import urllib.request as request
     import urllib.parse
+    import urllib.request as request
     from sys import argv
     values = {'email': argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('utf-8')
     req = request.Request(argv[1], data)
     with request.urlopen(req) as response:
-        html = response.read()
+        print(response.read().decode('utf-8'))
