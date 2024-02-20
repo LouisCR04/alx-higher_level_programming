c0da9a92f21cdc377a6bdbd3d0176116869c8783
LouisCR04 <louiskariuki04@gmail.com>
2024-02-20 19:15:29 +0300

    Task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Displays value in var X-Request-Id in header"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    req = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
