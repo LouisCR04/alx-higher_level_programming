4172bf16c29f21e83165de714becb6f7e525e893
LouisCR04 <louiskariuki04@gmail.com>
2024-03-28 12:33:00 +0300

    Pep 8 Compliance

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """locked Class"""
 
+
 class LockedClass:
-    """Prevents the user from dynamically creating 
-    new instance attributes, except if the new instance 
+    """Prevents the user from dynamically creating
+    new instance attributes, except if the new instance
     attribute is called first_name
     """
     __slots__ = ["first_name"]
