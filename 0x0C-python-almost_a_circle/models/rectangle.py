ae14a02d75bcd6d06cacdedff64878b0d213a7cc
LouisCR04 <louiskariuki04@gmail.com>
2024-03-30 16:55:06 +0300

    Fixed output errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,8 +85,9 @@ class Rectangle(Base):
 
     def __str__(self):
         """Overrides th __str__ method as directed"""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y}\
-            - {self.__width}/{self.__height}"
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
 
     def update(self, *args, **kwargs):
         """Assigns an argument to each attribute"""
