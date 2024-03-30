87cce85ce188f95a2ba1ffa3b42e1e90e95a88ed
LouisCR04 <louiskariuki04@gmail.com>
2024-03-30 14:00:22 +0300

    Added interpreter path

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python3
 class Base:
+    """
+    This is the Base model for
+    all other classes
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
         """Initializes a new Base
-        Args: id: identity number
+        Args: id (int): identity number
         """
         if id is not None:
             self.id = id
