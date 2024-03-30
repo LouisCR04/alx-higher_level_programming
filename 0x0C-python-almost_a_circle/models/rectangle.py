5077640a803318ee993b54820499baff8324df9d
LouisCR04 <louiskariuki04@gmail.com>
2024-03-30 15:30:45 +0300

    Back up

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,7 @@ Defines the Rectangle class
 """
 from models.base import Base
 
+
 class Rectangle(Base):
     """A rec class that inherits from base"""
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -14,7 +15,6 @@ class Rectangle(Base):
         self.__y = y
         super().__init__(id)
 
-
     @property
     def width(self):
         """Set/get the width of the Rectangle."""
@@ -22,7 +22,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if value is not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -35,7 +35,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if value is not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -48,7 +48,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if type(value) != int:
+        if value is not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -61,8 +61,49 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if type(value) != int:
+        if value is not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Returns Area of the Rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """Prints the rectangle using '#' character"""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
+
+    def __str__(self):
+        """Overrides th __str__ method as directed"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y}\
+            - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """Assigns an argument to each attribute"""
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.__width = arg
+                elif a == 2:
+                    self.__height = arg
+                elif a == 3:
+                    self.__x = arg
+                elif a == 4:
+                    self.__y = arg
+                a += 1
